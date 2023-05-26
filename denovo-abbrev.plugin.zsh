46c054e6c93b70c058ba7dc9e51193638d85104f
Shinnosuke Sawada <6warashi9@gmail.com>
2023-05-26 09:08:01 +0900

    feat: implement

--- denovo-abbrev.plugin.zsh
+++ denovo-abbrev.plugin.zsh
@@ -1,2 +1,12 @@
 typeset -gaU DENOVO_PATH
 DENOVO_PATH+=("${0:a:h}")
+
+function denovo-abbrev-expand() {
+	denovo-dispatch denovo-abbrev expand "$LBUFFER" "$RBUFFER" > /dev/null
+}
+zle -N denovo-abbrev-expand
+
+function denovo-abbrev-expand-and-accept-line() {
+	denovo-dispatch denovo-abbrev expand-and-accept-line "$LBUFFER" "$RBUFFER" > /dev/null
+}
+zle -N denovo-abbrev-expand-and-accept-line
--- denovo/denovo-abbrev/main.ts
+++ denovo/denovo-abbrev/main.ts
@@ -1,8 +1,17 @@
 import type { Denovo } from "https://deno.land/x/denovo_core@v0.0.4/mod.ts";
+import { assertString } from "https://deno.land/x/unknownutil@v2.1.1/mod.ts";
 import { z } from "https://deno.land/x/zod@v3.21.4/mod.ts";
 
 type Config = z.infer<typeof Config>;
-const Config = z.object({});
+type Snippet = z.infer<typeof Snippet>;
+const Snippet = z.object({
+  name: z.string().optional(),
+  keyword: z.string(),
+  snippet: z.string(),
+});
+const Config = z.object({
+  snippets: Snippet.array().optional(),
+});
 
 function isConfig(x: unknown): x is Config {
   return Config.safeParse(x).success;
@@ -14,6 +23,67 @@ export function main(denovo: Denovo): Promise<void> {
   if (isConfig(denovo.config)) {
     config = denovo.config;
   }
-  denovo.dispatcher = {};
+  denovo.dispatcher = {
+    async expand(lbuffer, rbuffer): Promise<void> {
+      lbuffer = lbuffer ?? "";
+      rbuffer = rbuffer ?? "";
+      assertString(lbuffer);
+      assertString(rbuffer);
+      await expand(denovo, lbuffer, rbuffer);
+    },
+    async "expand-and-accept-line"(lbuffer, rbuffer): Promise<void> {
+      lbuffer = lbuffer ?? "";
+      rbuffer = rbuffer ?? "";
+      assertString(lbuffer);
+      assertString(rbuffer);
+      await expand_and_accept(denovo, lbuffer, rbuffer);
+    },
+  };
   return Promise.resolve();
+}
+
+function selfinsert(denovo: Denovo): Promise<string> {
+  return denovo.eval("zle self-insert; zle redisplay");
+}
+
+async function expand(
+  denovo: Denovo,
+  lbuffer: string,
+  rbuffer: string,
+): Promise<boolean> {
+  if (config.snippets == null) {
+    await selfinsert(denovo);
+    return false;
+  }
+  if (/(^$|^\s)/.test(lbuffer) || !/(^$|^\s)/.test(rbuffer)) {
+    await selfinsert(denovo);
+    return false;
+  }
+  const tokens = lbuffer.split(/\s/);
+  if (tokens.length !== 1) {
+    await selfinsert(denovo);
+    return false;
+  }
+  const word = tokens[0];
+
+  for (const { keyword, snippet } of config.snippets) {
+    if (keyword !== word) {
+      continue;
+    }
+    await denovo.eval(`LBUFFER="${snippet} "; zle redisplay`);
+    return true;
+  }
+  await selfinsert(denovo);
+  return false;
+}
+
+async function expand_and_accept(
+  denovo: Denovo,
+  lbuffer: string,
+  rbuffer: string,
+): Promise<void> {
+  const succeeded = await expand(denovo, lbuffer, rbuffer);
+  if (succeeded) {
+    denovo.eval("zle accept-line; zle redisplay");
+  }
 }
