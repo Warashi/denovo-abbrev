898af83eba9e01fdcca774cf1deca073374c2c06
Shinnosuke Sawada-Dazai <shin@warashi.dev>
2026-01-17 15:36:06 +0900

    feat: migrate to new denovo.zsh

--- README.md
+++ README.md
@@ -1,42 +1,47 @@
 # Quick start
 
-denovo-abbrev is plugin of [denovo.zsh][denovo.zsh].
-you need to install [Deno][deno] and [denovo.zsh][denovo.zsh].
- 
+denovo-abbrev is plugin of [denovo.zsh][denovo.zsh]. you need to install
+[Deno][deno] and [denovo.zsh][denovo.zsh].
+
 [denovo.zsh]: https://github.com/Warashi/denovo.zsh/
 [deno]: https://deno.land/
 
 # Features
+
 - `denovo-abbrev-expand`: expand abbreviations
 - `denovo-abbrev-expand-and-accept-line`: expand abbreviations and accept line
 
 # Bindings example
+
 ```zsh
 bindkey ' '  denovo-abbrev-expand
 bindkey '^m' denovo-abbrev-expand-and-accept-line
 ```
 
 # Options
-You can set options in config file: `$XDG_CONFIG_HOME/denovo/config.toml` or `$HOME/.config/denovo/config.toml`.
-For example:
+
+You can set options in config file: `$XDG_CONFIG_HOME/denovo-abbrev/config.toml` or
+`$HOME/.config/denovo-abbrev/config.toml`. For example:
 
 ```toml
-[plugins.abbrev]
-[[plugins.abbrev.snippets]]
+[abbrev]
+[[abbrev.snippets]]
 name = "git"
 keyword = "g"
 snippet = "git"
 
-[[plugins.abbrev.snippets]]
+[[abbrev.snippets]]
 name = "ls -l"
 keyword = "ll"
 snippet = "ls -l"
 
-[[plugins.abbrev.snippets]]
+[[abbrev.snippets]]
 name = "ls -al"
 keyword = "la"
 snippet = "ls -al"
 ```
 
 # License
-The code follows MIT license written in [LICENSE][./LICENSE]. Contributors need to agree that any modifications sent in this repository follow the license.
+
+The code follows MIT license written in [LICENSE][./LICENSE]. Contributors need
+to agree that any modifications sent in this repository follow the license.
--- denovo/abbrev/main.ts
+++ denovo/abbrev/main.ts
@@ -1,64 +1,114 @@
-import type { Denovo } from "https://deno.land/x/denovo_core@v0.0.4/mod.ts";
-import { assertString } from "https://deno.land/x/unknownutil@v2.1.1/mod.ts";
-import { z } from "https://deno.land/x/zod@v3.21.4/mod.ts";
+import { parse as parseToml } from "@std/toml";
+import { join } from "@std/path";
+import { exists } from "@std/fs";
 
-type Config = z.infer<typeof Config>;
-type Snippet = z.infer<typeof Snippet>;
-const Snippet = z.object({
-  name: z.string().optional(),
-  keyword: z.string(),
-  snippet: z.string(),
+import { asOptional } from "@core/unknownutil/as/optional";
+import { ensure } from "@core/unknownutil/ensure";
+import { isArrayOf } from "@core/unknownutil/is/array-of";
+import { isObjectOf } from "@core/unknownutil/is/object-of";
+import { isString } from "@core/unknownutil/is/string";
+import { type PredicateType } from "@core/unknownutil/type";
+
+import { type Entrypoint } from "@warashi/denovo-core";
+
+const isSnippet = isObjectOf({
+  name: asOptional(isString),
+  keyword: isString,
+  snippet: isString,
 });
-const Config = z.object({
-  snippets: Snippet.array().optional(),
+
+const isAbbrev = isObjectOf({
+  snippets: asOptional(isArrayOf(isSnippet)),
 });
 
-function isConfig(x: unknown): x is Config {
-  return Config.safeParse(x).success;
-}
+const isConfig = isObjectOf({
+  abbrev: asOptional(isAbbrev),
+});
 
-let config: Config = {};
+type Config = PredicateType<typeof isConfig>;
 
-export function main(denovo: Denovo): Promise<void> {
-  if (isConfig(denovo.config)) {
-    config = denovo.config;
-  }
+// Cached config
+// undefined: not loaded yet
+// null: no config file
+// Config: loaded config
+let config: Config | null | undefined = undefined;
+
+export const main: Entrypoint = (denovo) => {
   denovo.dispatcher = {
-    async expand(lbuffer, rbuffer): Promise<void> {
-      lbuffer = lbuffer ?? "";
-      rbuffer = rbuffer ?? "";
-      assertString(lbuffer);
-      assertString(rbuffer);
-      const expanded = expand(lbuffer, rbuffer);
-      if (expanded) {
-        await denovo.eval(`LBUFFER='${expanded} '; zle redisplay`);
+    "expand": async (lbuffer, rbuffer) => {
+      if (config === undefined) {
+        await loadConfig();
+      }
+      if (config === null) {
+        await denovo.call("eval", `zle self-insert; zle redisplay`);
         return;
       }
-      await denovo.eval(`zle self-insert; zle redisplay`);
-    },
-    async "expand-and-accept-line"(lbuffer, rbuffer): Promise<void> {
-      lbuffer = lbuffer ?? "";
-      rbuffer = rbuffer ?? "";
-      assertString(lbuffer);
-      assertString(rbuffer);
-      const expanded = expand(lbuffer, rbuffer);
+      const expanded = expand(
+        ensure(config, isConfig),
+        ensure(lbuffer, isString),
+        ensure(rbuffer, isString),
+      );
       if (expanded) {
-        await denovo.eval(
+        await denovo.call(
+          "eval",
+          `LBUFFER='${expanded}'; zle self-insert; zle redisplay`,
+        );
+        return;
+      }
+      await denovo.call("eval", `zle self-insert; zle redisplay`);
+    },
+    "expand-and-accept-line": async (lbuffer, rbuffer) => {
+      if (config === undefined) {
+        await loadConfig();
+      }
+      if (config === null) {
+        await denovo.call("eval", `zle accept-line; zle redisplay`);
+        return;
+      }
+      const expanded = expand(
+        ensure(config, isConfig),
+        ensure(lbuffer, isString),
+        ensure(rbuffer, isString),
+      );
+      if (expanded) {
+        await denovo.call(
+          "eval",
           `LBUFFER='${expanded}'; zle accept-line; zle redisplay`,
         );
         return;
       }
-      await denovo.eval(`zle accept-line; zle redisplay`);
+      await denovo.call("eval", `zle accept-line; zle redisplay`);
     },
   };
   return Promise.resolve();
-}
+};
 
-function expand(
+const loadConfig = async () => {
+  const home = Deno.env.get("HOME");
+  const configHome = Deno.env.get("XDG_CONFIG_HOME");
+  if (home == null && configHome == null) {
+    config = null;
+    return;
+  }
+  const configPath = join(
+    configHome ?? join(ensure(home, isString), ".config"),
+    "denovo-abbrev",
+    "config.toml",
+  );
+  if (!(await exists(configPath))) {
+    config = null;
+    return;
+  }
+  const parsed = parseToml(await Deno.readTextFile(configPath));
+  config = ensure(parsed, isConfig);
+};
+
+const expand = (
+  config: Config,
   lbuffer: string,
   rbuffer: string,
-): string | undefined {
-  if (config.snippets == null) {
+) => {
+  if (config.abbrev?.snippets == null) {
     return undefined;
   }
   if (/(^$|^\s)/.test(lbuffer) || !/(^$|^\s)/.test(rbuffer)) {
@@ -70,11 +120,11 @@ function expand(
   }
   const word = tokens[0];
 
-  for (const { keyword, snippet } of config.snippets) {
+  for (const { keyword, snippet } of config.abbrev?.snippets ?? []) {
     if (keyword !== word) {
       continue;
     }
     return snippet;
   }
   return undefined;
-}
+};
